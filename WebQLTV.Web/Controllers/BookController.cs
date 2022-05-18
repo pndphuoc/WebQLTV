371e506b6a306d90d254ab2eccaa17d6645eacfd
DangQuang2001 <quangrc2001@gmail.com>
2022-05-18 11:23:50 +0700

    front end

---
+++ WebQLTV.Web/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebQLTV.Web.Controllers
+{
+    public class BookController: Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
