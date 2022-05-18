0ff2c2f0c8d450f3a6d9816d864c5362fdc59857
DangQuang2001 <quangrc2001@gmail.com>
2022-05-18 18:46:11 +0700

    Update đợt 3

--- WebQLTV.Web/Controllers/HistoryController.cs
+++ WebQLTV.Web/Controllers/HistoryController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace WebQLTV.Web.Controllers
 {
-    public class HistoryController
+    public class HistoryController: Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
