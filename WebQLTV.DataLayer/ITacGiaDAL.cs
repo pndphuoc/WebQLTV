c26d00149dad9ddc6ad4670d03e8764708dff348
pndphuoc <phamngocduyphuoc3132001@gmail.com>
2022-05-18 17:34:35 +0700

    update giao diện sách

---
+++ WebQLTV.DataLayer/ITacGiaDAL.cs
@@ -0,0 +1,15 @@
+﻿using QuanLyThuVIen.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebQLTV.DataLayer
+{
+    public interface ITacGiaDAL
+    {
+        List<TacGia> List(int MaSach);
+
+    }
+}
